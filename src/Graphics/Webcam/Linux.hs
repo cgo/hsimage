0e00c3bbde6b40b7e3fc99ce0cb712778fd7d320
Christian <github@goschs.de>
2012-05-13 20:00:43 +0200

    Added some more documentation.

--- src/Graphics/Webcam/Linux.hs
+++ src/Graphics/Webcam/Linux.hs
@@ -1,3 +1,18 @@
+{-| The Linux interface for accessing webcams via video4linux.
+This module uses the lower-level bindings to v4l to provide for a simple
+way to access a web camera from Haskell code using repa. 
+The module is meant to be used like shown in the file /example.hs/
+in the root of the package.
+For example:
+
+> main = runCam (grab >>= saveBmp "1.bmp" >> 
+>                grab >>= saveBmp "2.bmp" >> 
+>                grab >>= saveBmp "3.bmp") (Webcam 0)
+
+This example would take three pictures from the first webcam and
+store them in three files.
+-}
+
 module Graphics.Webcam.Linux 
        ( 
          Webcam (..)
